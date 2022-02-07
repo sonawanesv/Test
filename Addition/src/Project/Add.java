957ca53fe671a706a44c5fd7e5431da70e74afd1
HP <HP@LAPTOP-MBU8NU9S>
2022-02-07 18:12:24 +0530

    Addition program value from user

---
+++ Addition/src/Project/Add.java
@@ -0,0 +1,22 @@
+package Project;
+
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int num1, num2, sum;
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter First Value :");
+		num1 = sc.nextInt();
+		
+		System.out.println("Enter Second Value:");
+		num2 = sc.nextInt();
+		
+		sc.close();
+		sum=num1+num2;
+		System.out.println("sum of these numbers :"+sum);
+
+	}
+
+}
